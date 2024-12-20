3a2eb911816df5fecaed801a4fa757c0358a6f55
Chloe <chloe.l@nexusplay.tw>
2024-12-20 16:39:40 +0800

    fix: init

---
+++ src/questions.js
@@ -0,0 +1,202 @@
+export default  [
+  {
+    id: 'q1',
+    text: 'K-pop 歷史上第一個被公認的偶像團體是哪一個？',
+    answers: [
+      'H.O.T.', // 正解
+      'Shinhwa',
+      'Sechs Kies',
+      'Baby V.O.X.',
+    ],
+  },
+  {
+    id: 'q2',
+    text: '哪一年被稱為 "K-pop 大爆炸元年"，因為那年多個知名偶像團體出道？',
+    answers: [
+      '2007', // 正解
+      '2000',
+      '2012',
+      '2016',
+    ],
+  },
+  {
+    id: 'q3',
+    text: '防彈少年團（BTS）的第一首 Billboard Hot 100 冠軍歌曲是？',
+    answers: [
+      '"Dynamite"', // 正解
+      '"Butter"',
+      '"Fake Love"',
+      '"Boy With Luv"',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'Blackpink 的首張正規專輯名稱是什麼？',
+    answers: [
+      '"The Album"', // 正解
+      '"Square Up"',
+      '"Kill This Love"',
+      '"Born Pink"',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'TWICE 中的成員中，哪一位是日本籍？',
+    answers: [
+      'Sana', // 正解
+      'Tzuyu',
+      'Mina',
+      'Lisa',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'Stray Kids 的隊長是誰？',
+    answers: [
+      'Bang Chan', // 正解
+      'Felix',
+      'Hyunjin',
+      'Han',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'EXO 中哪一位成員在 2021 年正式退伍？',
+    answers: [
+      'Suho', // 正解
+      'D.O.',
+      'Xiumin',
+      'Baekhyun',
+    ],
+  },
+  {
+    id: 'q8',
+    text: 'Red Velvet 的哪一首歌曲以獨特的 "Zimzalabim" 舞蹈動作而聞名？',
+    answers: [
+      '"Zimzalabim"', // 正解
+      '"Red Flavor"',
+      '"Russian Roulette"',
+      '"Bad Boy"',
+    ],
+  },
+  {
+    id: 'q9',
+    text: '以下哪支舞蹈被公認為 K-pop 舞蹈史上最具挑戰性之一？',
+    answers: [
+      '"Sherlock" - SHINee', // 正解
+      '"TT" - TWICE',
+      '"Growl" - EXO',
+      '"Hard Carry" - GOT7',
+    ],
+  },
+  {
+    id: 'q10',
+    text: 'Blackpink 的粉絲名稱是什麼？',
+    answers: [
+      'Blink', // 正解
+      'Army',
+      'Once',
+      'Carat',
+    ],
+  },
+  {
+    id: 'q11',
+    text: 'GOT7 的粉絲名稱是？',
+    answers: [
+      'Ahgase', // 正解
+      'VIP',
+      'NCTzen',
+      'ELF',
+    ],
+  },
+  {
+    id: 'q12',
+    text: '哪一個團體是第一個在美國 Billboard 音樂獎（BBMAs）獲得獎項的 K-pop 團體？',
+    answers: [
+      'BTS', // 正解
+      'EXO',
+      'BLACKPINK',
+      'Super Junior',
+    ],
+  },
+  {
+    id: 'q13',
+    text: 'Girls\' Generation（少女時代）哪一首歌曲奪得了韓國 Gaon Chart Music Awards 的年度歌曲獎？',
+    answers: [
+      '"I Got a Boy"', // 正解
+      '"Gee"',
+      '"The Boys"',
+      '"Lion Heart"',
+    ],
+  },
+  {
+    id: 'q14',
+    text: 'Aespa 的概念融合了什麼元素？',
+    answers: [
+      '人類和 AI 世界', // 正解
+      '童話和魔法',
+      '時光旅行',
+      '動物變身',
+    ],
+  },
+  {
+    id: 'q15',
+    text: 'Super Junior 的粉絲燈棒顏色是？',
+    answers: [
+      '珍珠藍', // 正解
+      '薰衣草紫',
+      '天空藍',
+      '粉紅色',
+    ],
+  },
+  {
+    id: 'q16',
+    text: 'NCT 的子團 NCT 127 以哪一個城市命名？',
+    answers: [
+      '首爾', // 正解
+      '東京',
+      '上海',
+      '釜山',
+    ],
+  },
+  {
+    id: 'q17',
+    text: '哪位 solo 歌手是第一位登上 Coachella 主舞台的 K-pop 藝人？',
+    answers: [
+      'CL', // 正解
+      'G-Dragon',
+      'IU',
+      'Jennie',
+    ],
+  },
+  {
+    id: 'q18',
+    text: '"江南 Style" 的歌手 Psy 在 YouTube 上首度創造了什麼紀錄？',
+    answers: [
+      '第一個破 10 億觀看次數的影片', // 正解
+      '第一個破 5 億觀看次數的影片',
+      '第一個獲得金牌的影片',
+      '第一個擁有 1000 萬訂閱的韓國歌手',
+    ],
+  },
+  {
+    id: 'q19',
+    text: 'SEVENTEEN 的歌曲 "Very Nice" 是哪一個專輯的主打歌？',
+    answers: [
+      '"Love & Letter Repackage Album"', // 正解
+      '"Teen, Age"',
+      '"Going Seventeen"',
+      '"An Ode"',
+    ],
+  },
+  {
+    id: 'q20',
+    text: 'ITZY 的出道歌曲是什麼？',
+    answers: [
+      '"Dalla Dalla"', // 正解
+      '"Not Shy"',
+      '"Wannabe"',
+      '"ICY"',
+    ],
+  },
+];
